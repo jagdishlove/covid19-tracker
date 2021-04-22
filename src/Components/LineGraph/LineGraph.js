76aaf01463fddd182e7de5decb0ee2effc9b1317
jagdishlove <jagdish.funn@gmail.com>
2021-04-22 15:48:35 +0530

    Map

--- src/Components/LineGraph/LineGraph.js
+++ src/Components/LineGraph/LineGraph.js
@@ -1,106 +1,106 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
-import numeral from 'numeral';
+import axios from "axios";
+import numeral from "numeral";
 
-const options={
-    legend:{
-        display:false,
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
     },
-    elements:{
-        point:{
-            radius:0,
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
         },
-    },
-    maintainAspectRatio:false,
-    tooltips:{
-        mode:"index",
-        intersect:false,
-        callbacks:{
-            label: function(tooltipItem, data){
-                return numeral(tooltipItem.value).format("+0.0");
-            },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
         },
-    },
-    scales:{
-        xAxes:[
-            {
-                type:"time",
-                time:{
-                    format:"MM/DD/YY",
-                    tooltipFormat:"ll",  
-                },
-            }, 
-    
-        ],
-        yAxes:[
-            {
-                gridLines:{
-                    display:false,
-                },
-                ticks:{
-                    callback:function(value,index,values){
-                        return numeral(value).format("0a");
-                    }
-                }
-            }
-        ]
-    },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format();
+          },
+        },
+      },
+    ],
+  },
+};
 
-}
+const buildChartData = (data, casesType) => {
+  let chartData = [];
+  let lastDataPoint;
 
-const  LineGraph = ({}) => {
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      let newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+
+  return chartData;
+};
+
+function LineGraph({casesType="cases"}) {
   const [data, setData] = useState({});
 
-  const buildChartData = (data, casesType = "cases") => {
-    const chartData = [];
-    let lastDataPoint;
-    for(let date in data.cases){
-      if (lastDataPoint) {
-        const newDataPoint = {
-          x: date,
-          y: data["cases"][date] - lastDataPoint,
-        };
-        chartData.push(newDataPoint);
-      }
-      lastDataPoint = data[casesType][date];
-    };
-    return chartData;
-  };
-
   useEffect(() => {
-      const fetchData=async()=>{
-        await  axios
+    const fetchData = async () => {
+      await axios
         .get("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-        .then((response) => {
-          const chartData = buildChartData(response.data);
+        .then((response) => response.data)
+        .then((data) => {
+          let chartData = buildChartData(data,"cases");
           setData(chartData);
         });
+    };
 
-      };
-
-      fetchData();
-   
-  }, []);
+    fetchData();
+  }, [casesType]);
 
   return (
-    <div className="Graph">
-      {/* https://disease.sh/v3/covid-19/historical/all?lastdays=120 */}
-    <h1>I am a graph</h1>
-    {data?.length > 0 &&(
-
-      <Line
-      options={options}
-        data={{
-          datasets: [{
-              backgroundColor:"rgba(204, 16, 52, 0.5)",
-              borderColor:"#CC1034",
-              data: data
-            }],
-        }}
-       
-      />
-    )}
+    <div>
+      <h1>I am a graaph</h1>
+      {data?.length > 0 && (
+        <Line
+          options={options}
+          data={{
+            datasets: [
+              {
+                backgroundColor: "rgba(204, 16, 52, 0.5)",
+                
+                borderColor: "#CC1034",
+                borderWidth:1,
+                data: data,
+              },
+            ],
+          }}
+        />
+      )}
     </div>
   );
 }
